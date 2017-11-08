90d1d52a3e6062b288955a3235d139bc25d48401
Sipacheva <so.888@mail.ru>
2017-11-08 17:12:38 +0300

    Добавлен тест для модификации групп GroupModificationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.adressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.adressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification() {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test7", "test8", "test9"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
